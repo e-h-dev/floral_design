1d347ca1c703eaa1bee383d881065949e4e24b7c
Eli Hammond <ebaal8226@gmail.com>
2024-11-10 12:34:38 +0000

    Add add to basket view

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,14 +1,27 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
-def basket_contents(requst):
+def basket_contents(request):
 
     basket_items = []
     total = 0
-    products_count = 0
+    product_count = 0
+    basket = request.session.get('basket', {})
+
+    for item_id, quantity in basket.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_AMOUNT:
-        if total < 50:
+        if total < 40:
             delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         else:
              delivery = total * Decimal(settings.HIGH_DELIVERY_PERCENTAGE / 100)
