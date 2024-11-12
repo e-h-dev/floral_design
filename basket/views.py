0f26def1a0cc0d3c16b9de100fde75612966a4ce
Eli Hammond <ebaal8226@gmail.com>
2024-11-12 15:09:35 +0000

    Add to messages

--- basket/views.py
+++ basket/views.py
@@ -20,9 +20,11 @@ def add_to_basket(request, item_id):
 
     if item_id in list(basket.keys()):
         basket[item_id] += quantity
+        messages.success(request, f'You have Successfully Added more {product.name}, to your Basket')
+
     else:
         basket[item_id] = quantity
-    messages.success(request, f'You have Successfully Added {product.name}, to your Basket')
+        messages.success(request, f'You have Successfully Added {product.name}, to your Basket')
 
     request.session['basket'] = basket
     return redirect(redirect_url)
@@ -30,11 +32,13 @@ def add_to_basket(request, item_id):
 def adjust_basket(request, item_id):
     """ adds items and quantity of items to basket"""
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     basket = request.session.get('basket', {})
 
     if quantity > 0:
         basket[item_id] = quantity
+        messages.success(request, f'You have Successfully Added more {product.name}, to your Basket')
     else:
         basket.pop(item_id)
 
@@ -45,14 +49,17 @@ def adjust_basket(request, item_id):
 def remove_from_basket(request, item_id):
     """ removes items from basket"""
 
+    product = Product.objects.get(pk=item_id)
     try:
         basket = request.session.get('basket', {})
         
         basket.pop(item_id)
+        messages.success(request, f'You have Successfully Removed {product.name}, from your Basket')
 
         request.session['basket'] = basket
         return HttpResponse(status=200)
     except Exception as e:
+        messages.error(request, f'Error removing {e}')
         return HttpResponse(status=200)
 
 
