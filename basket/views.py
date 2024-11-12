68818ec533fb1cde00c9e69dc8b65311dde67c83
Eli Hammond <ebaal8226@gmail.com>
2024-11-12 14:54:24 +0000

    Add and style display messages

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+
+from products.models import Product
 
 # Create your views here.
 
@@ -10,6 +13,7 @@ def view_basket(request):
 def add_to_basket(request, item_id):
     """ adds items and quantity of items to basket"""
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
@@ -18,6 +22,7 @@ def add_to_basket(request, item_id):
         basket[item_id] += quantity
     else:
         basket[item_id] = quantity
+    messages.success(request, f'You have Successfully Added {product.name}, to your Basket')
 
     request.session['basket'] = basket
     return redirect(redirect_url)
