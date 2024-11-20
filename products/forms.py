4e8e96522e26dd784674ba185db0b7ef4b45fed2
Eli Hammond <ebaal8226@gmail.com>
2024-11-20 13:14:05 +0000

    Update class for add products form

--- products/forms.py
+++ products/forms.py
@@ -7,11 +7,11 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = '__all__'
 
-    def __Iinit__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        categories = category.objects.all()
+        categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'main-print rounded-2'
+            field.widget.attrs['class'] = 'shadow form-back'
