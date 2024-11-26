7525c56f9add73d447d45c9ee4cc2d80e83cc346
Eli Hammond <ebaal8226@gmail.com>
2024-11-26 11:54:09 +0000

    Update heroku deployment and allowed hosts

--- floral_design/settings.py
+++ floral_design/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['floral-design.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['floral-design-abd35de492a3.herokuapp.com', 'localhost']
 
 #8000-ehdev-floraldesign-e9576ycje1s.ws.codeinstitute-ide.net
 
